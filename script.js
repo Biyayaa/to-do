3402699bca69c5b9044b06621f3a5ab3ba8c3fa8
Biyayaa <onaleye.ap@gmail.com>
2023-04-09 20:27:40 +0100

    shows a message when a task is added or deleted

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function displayTasks() {
     arr.forEach((el, i)=>{
         task.innerHTML += `
         <h2 id="event" class="event">
-            ${i +1} ${el}
+            ${i+1+"."} ${el}
             <div class="edit-delete">
                 <button onclick="editTask(${i})" id="edit-btn">Edit</button>
                 <button onclick="deleteTask(${i})" id="delete-btn">Delete</button>
@@ -23,25 +23,25 @@ function displayTasks() {
 
 function add() {
     if (input.value == "") {
-        // alert("Type something")
-        taskStatus.innerHTML = "<p id='error-message'> Add a task first</p>"
-        // setTimeout(() => {
-        //     taskStatus.style.display = "none" 
-        // }, 10000);
+        taskStatus.innerHTML = "<p id='error-message'> Add a task first!!!</p>"
     }
     else{
         arr.push(input.value);
         displayTasks();
         taskStatus.innerHTML =  "<p id='sucess-message'>task updated successfully!!!</p>"
         setTimeout(() => {
-            taskStatus.style.display = "none" 
-        }, 5000);
+            taskStatus.innerHTML = "";
+        }, 3000);
     }
 }
 
 function deleteTask(index) {
     arr.splice(index, 1);
     displayTasks();
+    taskStatus.innerHTML =  "<p id='error-message'>a task has been deleted!!</p>"
+        setTimeout(() => {
+            taskStatus.innerHTML = "";
+        }, 3000);
 }
 
 
